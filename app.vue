6fda804657345e4204ced695d308d916f4768036
wen-3 <sd63324@gmail.com>
2024-11-21 21:30:06 +0800

    useFetch、useAsyncData、$fetch

--- app.vue
+++ app.vue
@@ -1,6 +1,21 @@
+<script setup>
+import axios from 'axios';
+
+// const { data, refresh } = await useFetch("https://nuxr3.zeabur.app/api/v1/rooms/");
+
+const { data, refresh } = await useAsyncData("index", async () => {
+  const res = await axios.get("https://nuxr3.zeabur.app/api/v1/rooms/");
+  return res.data;
+})
+
+const refreshData = () => {
+  refresh();
+}
+
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <button @click="refreshData">重新抓取網頁資料</button>
+
+  <div>{{ data }}</div>
 </template>
