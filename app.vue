4713ea8df452e40d9f729f2b12c7d8a528efd65a
wen-3 <sd63324@gmail.com>
2024-11-23 23:12:38 +0800

    多個 api 請求
    useAsyncData + Promise.allSettled

--- app.vue
+++ app.vue
@@ -1,21 +1,41 @@
 <script setup>
-import axios from 'axios';
+// import axios from 'axios';
 
 // const { data, refresh } = await useFetch("https://nuxr3.zeabur.app/api/v1/rooms/");
 
-const { data, refresh } = await useAsyncData("index", async () => {
-  const res = await axios.get("https://nuxr3.zeabur.app/api/v1/rooms/");
-  return res.data;
-})
+// 多個 api 請求
+const { data } = await useAsyncData("info", async () => {
+  const apis = [
+    $fetch(`https://nuxr3.zeabur.app/api/v1/rooms/`),
+    $fetch(`https://nuxr3.zeabur.app/api/v1/rooms/66b6e81931c83030e03b537d`)
+  ];
 
-const refreshData = () => {
-  refresh();
-}
+  const all = await Promise.allSettled(apis);
 
+  // 只處理成功的請求結果
+  const successfulResponses = all
+    .filter(res => res.status === 'fulfilled') // 過濾出成功的結果
+    .map(res => res.value); // 提取成功的資料
+
+  return successfulResponses;
+});
+
+console.log(data.value);
+
+// const refreshData = () => {
+//   refresh();
+// }
 </script>
 
 <template>
-  <button @click="refreshData">重新抓取網頁資料</button>
+  <!-- <button @click="refreshData">重新抓取網頁資料</button> -->
+  <!-- <div>{{ data[0].result }}</div> -->
 
-  <div>{{ data }}</div>
+  <div v-for="(item) in data[0].result" :key="_id">
+    房間名：{{ item.name }}
+    <br>
+    描述：{{ item.description }}
+    <hr>
+  </div>
+  
 </template>
