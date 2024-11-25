ebd2655e2a99ebdedb204961b91e1b5b61c468a7
wen-3 <sd63324@gmail.com>
2024-11-25 16:29:52 +0800

    嵌套式 Router
    內嵌頁面

--- pages/about.vue
+++ pages/about.vue
@@ -7,6 +7,29 @@
     <NuxtLink to="/">回到首頁</NuxtLink>
     <NuxtLink to="/address">聯絡地址</NuxtLink>
   </div>
+
+  <br>
+
+  <div class="nav">
+    <NuxtLink to="/about">about index</NuxtLink>
+    <NuxtLink to="/about/a">about a</NuxtLink>
+    <NuxtLink to="/about/b">about b</NuxtLink>
+  </div>
+
+  <div class="box_page">
+    <NuxtPage></NuxtPage>
+  </div>
 </template>
 
-<style scoped></style>
+<style>
+  .nav > a {
+    display: block;
+    margin: 0 10px;
+  }
+
+  .box_page{
+    width: 800px;
+    height: 400px;
+    background-color: #ddd ;
+  }
+</style>
