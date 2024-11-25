96b7c574e1b6b2b4838eae366ff133b3615e0cc6
wen-3 <sd63324@gmail.com>
2024-11-25 16:14:39 +0800

    useRoute() & useRouter()

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,12 @@
-<script setup></script>
+<script setup>
+  const route = useRoute();
+  const router = useRouter();
+
+  console.log(route);
+  const gotoAndPage = (path) => {
+    router.push({ path });
+  };
+</script>
 
 <template>
   <div>
@@ -7,6 +15,8 @@
     <NuxtLink to="/about">關於我們</NuxtLink>
     <NuxtLink to="/address">聯絡地址</NuxtLink>
   </div>
+  <hr>
+  <button @click="gotoAndPage('/about')">切換至 about 頁面</button>
 </template>
 
 <style scoped></style>
