13da02e705bf22bb198170ec3c1ba23401191ab9
wen-3 <sd63324@gmail.com>
2024-11-25 20:54:28 +0800

    局部性動態路由
    前綴字-[xxx]

--- pages/index.vue
+++ pages/index.vue
@@ -15,6 +15,7 @@
     <NuxtLink to="/about">關於我們</NuxtLink>
     <NuxtLink to="/address">聯絡地址</NuxtLink>
     <NuxtLink to="/courses">課程列表</NuxtLink>
+    <NuxtLink to="/user-mike">使用者頁面</NuxtLink>
   </div>
   <hr>
   <button @click="gotoAndPage('/about')">切換至 about 頁面</button>
