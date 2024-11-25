d00ca3765d6757f51f9066995d6340b386125bef
wen-3 <sd63324@gmail.com>
2024-11-25 21:06:49 +0800

    remove
    移除用不到的檔案與程式碼

--- pages/courses.vue
+++ pages/courses.vue
@@ -1,17 +1,7 @@
 <script setup>
-const id = ref();
-const router = useRouter();
-
-const gotoAndPage = (path) => {
-    router.push({ path });
-    id.value = '';
-};
 </script>
 
 <template>
-    <input type="text" v-model="id">
-    <button @click="gotoAndPage(`/courses/${id}`)">切換至單一課程</button>
-    <hr>
     <NuxtPage></NuxtPage>
 </template>
 
